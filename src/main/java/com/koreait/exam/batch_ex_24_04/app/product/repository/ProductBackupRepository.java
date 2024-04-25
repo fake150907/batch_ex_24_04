6b24aa81cc9263a117adcd52547a4e2c56d34fe4
fake150907 <yungayeon223@gmail.com>
2024-04-25 13:52:30 +0900

    상품과 상품옵션의 주요 데이터를 정산 테이블의 컬럼으로 적용, 서비스에 영향을 덜 끼치도록

--- src/main/java/com/koreait/exam/batch_ex_24_04/app/product/repository/ProductBackupRepository.java
+++ src/main/java/com/koreait/exam/batch_ex_24_04/app/product/repository/ProductBackupRepository.java
@@ -1,5 +1,6 @@
 package com.koreait.exam.batch_ex_24_04.app.product.repository;
 
+import com.koreait.exam.batch_ex_24_04.app.product.entity.Product;
 import com.koreait.exam.batch_ex_24_04.app.product.entity.ProductBackup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
